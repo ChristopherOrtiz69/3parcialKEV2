fd0417c24487db51facfda42e677c199841e797c
ChristopherOrtiz69 <christopher.villafanet@gmail.com>
2025-05-11 13:46:00 -0600

    commit

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class FollowTarget : MonoBehaviour
-{
+{   
     public Transform target;
     public Vector3 offset = new Vector3(0, 0, -5);
     public float smoothSpeed = 5f;
